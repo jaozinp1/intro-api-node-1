4a979b18b2f09756e270dc421b78a67a10fdeeb7
LucasGodoi000 <lucas_mammana_godoi@hotmail.com>
2025-03-27 20:40:03 -0300

    aula postila 2 27/03
    coisamo legal

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express'); 
 const cors = require('cors');
 
@@ -6,7 +7,7 @@ const app = express();
 app.use(cors()); 
 app.use(express.json()); 
 
-const porta = 3333;
+const porta = process.env.PORT || 3333;
 
 app.listen(porta, () => {
     console.log(`Servidor iniciado em http://localhost:${porta}`);
