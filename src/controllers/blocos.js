3260f811353bd1a20102a286574983f5f4f87795
jaozinp1 <joaohenriqeu23@gmail.com>
2025-04-03 20:35:53 -0300

    inicio das minhas tabelas
    do banco de dados

---
+++ src/controllers/blocos.js
@@ -0,0 +1,72 @@
+const db = require('../database/connection');
+
+module.exports ={
+    async listablocos (request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de blocos.',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Listagem de usuários.',
+                dados: error.message
+            
+            });
+        }   
+    },
+
+    async cadastrarblocos (request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Cadastrar bloco.',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Listagem de usuários.',
+                dados: error.message
+            
+            });
+        }   
+    },
+
+    async editarblocos (request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Editar blocos.',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Listagem de usuários.',
+                dados: error.message
+            
+            });
+        }   
+    },
+    async apagarblocos (request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'apagar blocos.',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                mensagem: 'Erro na Listagem de usuários.',
+                dados: error.message
+            
+            });
+        }   
+    },
+}
+
+  
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const usuarioApartamentoController = require('../controllers/usuarioApartamento');
+const Controller = require('../controllers/blocos');
 
-router.get('/usuarioApartamento', usuarioApartamentoController.listaUsuariosApartamento);
+router.get('/blocos', blocosController.listablocos);
+router.post('/blocos', blocosController.cadastrarblocos);
+router.patch('/blocos', blocosController.editarblocos);
+router.delete('/blocos', blocosController.apagarblocos);
 
 module.exports = router;
